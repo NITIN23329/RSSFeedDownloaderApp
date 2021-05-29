795ca1b5649d959b3d48527bb1b107f5fe10f28f
NITIN KUMAR DAS <NITIN23329@GMAIL.COM>
2021-05-29 13:13:33 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RSSFeedDownloaderApp
+ 
---
+++ app/src/main/java/com/nitin/rssfeeddownloaderapp/MainActivity.java
@@ -0,0 +1,80 @@
+package com.nitin.rssfeeddownloaderapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.security.auth.login.LoginException;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: Starting Async Task");
+        DownloadDataTask task = new DownloadDataTask();
+        task.onPostExecute("a valid url");
+        Log.d(TAG, "onCreate: ending onCreate");
+    }
+    private  class DownloadDataTask extends AsyncTask<String,Void,String>{
+        // An asynchronous task is defined by a computation that runs on a background thread and whose result is published on the UI thread.
+        // This helps us to run our task in background(like downloading) in parallel with the app so that for heavy tasks, app don't get freeze.
+        // Class AsyncTask<Params,Progress,Result> is a abstract class having 3 parameters.
+        // 1st parameter: The data type of parameters upon which we work, in our case we will send url as a String.
+        // 2nd parameter: The data type of progress which is going to be displaced, Void means we don't want to display our progress.
+        // 3rd parameter: The data type of our result, in our case it would be xml
+
+        private static final String TAG = "DownloadDataTask";
+        @Override   // after completion of doInBackground(), it will be called by passing the return value of doInBackground()
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d(TAG, "onPostExecute: parameter is: " + s);
+        }
+
+        @Override   // this runs in other thread asynchronously
+        protected String doInBackground(String ... str) {
+            // str is treated as an array
+            Log.d(TAG, "doInBackground: starts with: "+str[0]);
+            // we will only do the task for 1st element in str only
+            String rssFeed = downloadXML(str[0]);
+            if(rssFeed==null) Log.e(TAG, "doInBackground: can't download the rssFeed");
+            return rssFeed;
+        }
+
+        private String downloadXML(String urlPath){
+            StringBuilder xmlResult = new StringBuilder();
+            try{
+                URL url = new URL(urlPath);
+                HttpURLConnection connection = (HttpURLConnection)url.openConnection(); // opening connection with urlPath
+                //when we connect with a http, server gives us response code like 404(Can not found)
+                int responseCode = connection.getResponseCode();
+                Log.d(TAG, "downloadXML: the response code: "+ responseCode);
+
+                InputStream inputStream = connection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);  // buffered reader is used as it is fast.
+
+                bufferedReader.close();
+
+            }catch (MalformedURLException e){
+                Log.e(TAG, "downloadXML: Invalid URL: "+e.getMessage() );
+            }catch (IOException e){
+                Log.e(TAG, "downloadXML: Got IOException: "+e.getMessage());
+            }
+
+        }
+    }
+
+}
